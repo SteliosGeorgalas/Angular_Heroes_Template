fd1dd0d4d3154a7a431d36dbef46ded1d4e74a9c
sgeorgalas <sgeorgalas@ubitech.eu>
2022-12-19 14:41:35 +0200

    Heroe Details

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Hero } from '../hero';
+import { Heroes } from '../mock-heroes';
 
 @Component({
   selector: 'app-hero-details',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.css']
 })
-export class HeroDetailsComponent {
+
+export class HeroDetailsComponent implements OnInit {
+
+  hero!: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.hero = this.getHero(params['id'])!;
+    })
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  getHero(id: number) {
+    return Heroes.find(x => x.id == id);
+  }
 
 }
