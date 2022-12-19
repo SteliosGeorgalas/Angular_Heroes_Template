a6664f297a1213bab865e2e621eedffd5a4ac41f
sgeorgalas <sgeorgalas@ubitech.eu>
2022-12-19 13:47:57 +0200

    Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { Heroes } from '../mock-heroes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  heroes!: Hero[];
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.heroes = Heroes.slice(0, 4)
+  }
+
+  onSelect(hero: Hero) {
+    this.router.navigate(['/details', hero.id])
+  }
 
 }
